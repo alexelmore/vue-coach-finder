552ba6f338015b2e22e6a34506ffeb89097addba
aelmore49 <alexelmore49@gmail.com>
2022-05-27 06:57:25 -0500

    move form for coach registration into its own component

--- src/Pages/coaches/CoachRegistration.vue
+++ src/Pages/coaches/CoachRegistration.vue
@@ -2,126 +2,20 @@
   <section>
     <BaseCard>
       <h2>Become a Coach!</h2>
-      <form class="form-control" @submit.prevent="registerCoach">
-        <div class="field">
-          <label for="firstName">First Name</label>
-          <input required id="firstName" type="text" v-model="firstName" />
-        </div>
-        <div class="field">
-          <label for="lastName">Last Name</label>
-          <input required id="lastName" type="text" v-model="lastName" />
-        </div>
-
-        <div class="field">
-          <label for="rate">Hourly Rate</label>
-          <input required id="rate" type="number" v-model.number="rate" />
-        </div>
-
-        <div class="field">
-          <h3>Type of Coach:</h3>
-          <input
-            type="checkbox"
-            id="frontend"
-            name="frontend"
-            value="frontend"
-            @change="addArea"
-          />
-          <label for="frontend"> I am a frontend coach</label><br />
-
-          <input
-            type="checkbox"
-            id="backend"
-            name="backend"
-            value="backend"
-            @change="addArea"
-          />
-          <label for="backend"> I am a backend coach</label><br />
-          <input
-            type="checkbox"
-            id="career"
-            name="career"
-            value="career"
-            @change="addArea"
-          />
-          <label for="career"> I am a career coach</label><br />
-        </div>
-        <div>
-          <p><label for="description">Desciption:</label></p>
-          <textarea
-            id="description"
-            name="description"
-            rows="4"
-            cols="100"
-            placeholder="Tell us about yourself"
-            v-model="description"
-          ></textarea>
-        </div>
-        <div class="field">
-          <BaseButton mode="outline">Register Now!</BaseButton>
-        </div>
-      </form>
+      <CoachForm />
     </BaseCard>
   </section>
   {{ this.areas }}
 </template>
 
 <script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
 export default {
   name: 'CoachRegistration',
   data() {
-    return {
-      firstName: '',
-      lastName: '',
-      rate: null,
-      description: '',
-      id: '',
-      areas: [],
-    };
+    return {};
   },
-  methods: {
-    registerCoach() {
-      let id = `${this.firstName[0]}-${this.lastName[0]}`;
-      if (this.areas.length > 0) {
-        let newCoach = {
-          id,
-          firstName: this.firstName,
-          lastName: this.lastName,
-          description: this.description,
-          hourlyRate: this.rate,
-          areas: this.areas,
-        };
-        console.log('coach to add:', newCoach);
-        this.clearFormFields();
-      } else {
-        alert('Please select at least one type of coach');
-      }
-    },
-    addArea(e) {
-      const id = e.target.id;
-      const hasArea = e.target.checked;
-      if (hasArea) {
-        if (!this.areas.includes(id)) {
-          this.areas.push(id);
-        }
-      } else {
-        if (this.areas.includes(id)) {
-          let filteredArray = this.areas.filter((area) => area !== id);
-          this.areas = filteredArray;
-        }
-      }
-    },
-    clearFormFields() {
-      this.firstName = '';
-      this.lastName = '';
-      this.description = '';
-      this.rate = null;
-      this.areas = [];
-      let checkboxes = document.querySelectorAll('input[type=checkbox]');
-      checkboxes.forEach((check) => {
-        check.checked = false;
-      });
-    },
-  },
+  components: [CoachForm],
 };
 </script>
 
