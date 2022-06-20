258d344cf693e5ae9e0ea0c99de68954b5d36f3c
aelmore49 <alexelmore49@gmail.com>
2022-06-20 10:22:19 -0500

    add logic and functionality to get request messages and display them in the RequestReceived component

--- src/Pages/requests/RequestReceived.vue
+++ src/Pages/requests/RequestReceived.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div v-if="hasRequests">
+  <div v-if="!error">
+    <div v-if="isLoading" class="request-spinner"><BaseSpinner /></div>
+    <div v-else-if="readyToGo">
       <ul v-for="request in requests" :key="request.id">
         <RequestItem :request="request" />
       </ul>
@@ -11,6 +12,15 @@
       </BaseCard>
     </div>
   </div>
+  <div v-else>
+    <BaseDialog
+      @close="() => (this.error = null)"
+      :show="!!error"
+      title="An Error Occurred"
+    >
+      <p>{{ error }}</p>
+    </BaseDialog>
+  </div>
 </template>
 
 <script>
@@ -19,6 +29,15 @@ import RequestItem from '../../components/requests/RequestItem.vue';
 export default {
   name: 'RequestReceived',
   components: { RequestItem },
+  data() {
+    return {
+      error: null,
+      isLoading: false,
+    };
+  },
+  created() {
+    this.loadRequests();
+  },
   computed: {
     ...mapGetters({
       requests: 'requests/getRequests',
@@ -26,6 +45,24 @@ export default {
     }),
     coachId() {
       return this.$route.params.id;
+    },
+    readyToGo() {
+      return !this.isLoading && this.requests.length;
+    },
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        const data = await this.$store.dispatch('requests/fetchRequests');
+        console.log(data);
+        this.isLoading = false;
+      } catch (err) {
+        console.log(err.message);
+        this.error =
+          'Unable to load request messages. Please try again at a later time';
+        this.isLoading = false;
+      }
     },
   },
 };
@@ -43,5 +80,8 @@ ul {
   margin: 2rem auto;
   padding: 0;
   max-width: 30rem;
+}
+.request-spinner {
+  margin-top: 2.5rem;
 }
 </style>
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -3,12 +3,13 @@ export default {
         // Current User Id
         const userId = context.rootGetters.userId;
 
+        // Construct request object to send with post request 
         const newRequest = {
             id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             userMessage: payload.message
         }
+
         // Setup post request to add coach to FB data
         const response = await fetch(`https://vue-coach-bc6a2-default-rtdb.firebaseio.com/requests/${userId}.json`, {
             method: 'POST',
@@ -18,10 +19,43 @@ export default {
             //make sure to serialize your JSON body
             body: JSON.stringify(newRequest)
         })
+        // const data = await response.json()
+        try {
+            if (response.ok && response.status === 200) {
+                // Call addRequest mutation to commit data to state
+                context.commit('addRequest', { ...newRequest })
+            } else {
+                throw new Error('Something went wrong')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    async fetchRequests(context) {
+        // Current User Id
+        const userId = context.rootGetters.userId;
+
+        // Setup for sending GET request for the coaches stored in our FB DB.
+        const response = await fetch(`https://vue-coach-bc6a2-default-rtdb.firebaseio.com/requests/${userId}.json`)
+
+        // Parse returned data into json format
+        const data = await response.json()
 
         try {
             if (response.ok && response.status === 200) {
-                context.commit('addRequest', newRequest)
+                const requests = []
+                for (const key in data) {
+                    const request = {
+                        coachId: key,
+                        id: data[key].firstName,
+                        userEmail: data[key].userEmail,
+                        userMessage: data[key].userMessage
+                    }
+                    requests.push(request)
+                }
+                // Commit requests to state via fetchRequests mutation
+                context.commit('fetchRequests', requests)
             } else {
                 throw new Error('Something went wrong')
             }
