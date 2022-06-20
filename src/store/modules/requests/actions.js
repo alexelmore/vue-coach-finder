f6439ad6ab7b2494f032c4b74969aa8fea0ca22e
aelmore49 <alexelmore49@gmail.com>
2022-06-20 13:08:46 -0500

    add id property to newRequest object, using the name field that gets returned back to us in the responseData object

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -23,11 +23,13 @@ export default {
         // Store response data and parse it into json format
         const responseData = await response.json()
 
+        // Add the id field to the newRequest object and set it to the value that gets returned back in the name field of the responseData object
+        newRequest.id = responseData.name
 
         try {
             if (response.ok && response.status === 200) {
                 // Call addRequest mutation, passing it an object contructed from a speard out newRequest object and the id returned back from the responseData object
-                context.commit('addRequest', { ...newRequest, id: responseData.name })
+                context.commit('addRequest', newRequest)
             } else {
                 throw new Error('Something went wrong')
             }
